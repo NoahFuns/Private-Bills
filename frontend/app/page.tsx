9c038444b5df8071d9563b465f72d1947b579402
Je12654rryDora <filbeelowell288@gmail.com>
2025-11-06 20:52:29 +0800

    Add home page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { PrivateBillsDemo } from "@/components/PrivateBillsDemo";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-col gap-8 items-center w-full px-3 md:px-0">
+        <PrivateBillsDemo />
+      </div>
+    </main>
+  );
+}
+
+
